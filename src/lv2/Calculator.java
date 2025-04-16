c9a90c7fab61927322cb75b3a664f7b578a4f3f3
DongGeun <kk506kk@naver.com>
2025-04-16 16:53:42 +0900

    LV2 완료

--- src/lv2/Calculator.java
+++ src/lv2/Calculator.java
@@ -1,2 +1,64 @@
-package lv2;public class Calculator {
+package lv2;
+
+public class Calculator {
+    
+    // 속성 - 필드 (캡슐화)
+
+    private int firstNum; // 입력 받을 숫자 1
+    private int secondNum; // 입력 받을 숫자 2
+    private char operator; // 사칙 연산 기호
+
+
+
+    // setter
+    public void setFirstNum (int firstNum){
+        this.firstNum = firstNum;
+    }
+
+    public void setSecondNum (int secondNum){
+        this.secondNum = secondNum;
+    }
+
+    public void setOperator(char operator){
+        this.operator = operator;
+    }
+
+    // getter
+    public int getFirstNum (){
+        return firstNum;
+    }
+
+    public int getSecondNum (){
+        return secondNum;
+    }
+
+    public char getOperator (){
+        return operator;
+    }
+
+    // 계산하고 출력하는 메서드
+    public void calculator(int firstNum, int secondNum , char operator){
+        // 사칙연산 기호을 입력 받았을 경우
+        switch (operator){
+            case '+':
+                System.out.println(firstNum + " + " + secondNum + " = " + (firstNum + secondNum) );
+                break;
+            case '-':
+                System.out.println(firstNum + " - " + secondNum + " = " + (firstNum - secondNum) );
+
+                break;
+            case '*':
+                System.out.println(firstNum + " * " + secondNum + " = " + (firstNum * secondNum) );
+                break;
+            case '/':
+                System.out.println(firstNum + " / " + secondNum + " = " + (firstNum / secondNum) );
+                break;
+
+        }
+
+
+    }
+
+
+
 }
--- src/lv2/Main.java
+++ src/lv2/Main.java
@@ -1,7 +1,69 @@
 package lv2;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        // 입력 받은 값을 활용하기 위해 Scanner 사용
+        Scanner sc  = new Scanner(System.in);
+
+        // 인스턴스(객체) 생성
+        Calculator calculator = new Calculator();
+
+
+        while(true){
+            try{
+                // 양의 정수(0포함) 입력받기
+                System.out.print("1번째 숫자를 입력해주세요");
+                int firstNum = sc.nextInt();
+                System.out.print("2번째 숫자를 입력해주세요");
+                int secondNum = sc.nextInt();
+
+                // 입력 받은 숫자가 0보다 작을 경우
+                if (firstNum < 0 || secondNum < 0) {
+                    System.out.println("숫자를 0이상 입력해주세요!!");
+                    continue;
+                }else{
+                    boolean isTrue = true;
+                    while(isTrue){
+                        // 사칙연산 기호 입력 받기
+                        System.out.print("사칙연산 기호를 입력해주세요");
+                        char operator = sc.next().charAt(0);
+
+                        if(operator == '/' && secondNum ==0){
+                            System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력 될 수없습니다.");
+                            System.out.println("사칙연산 기호를 다시 입력해주세요");
+                            continue;
+                        } else if (!(operator == '+' || operator == '-' || operator == '*' || operator == '/')) {
+                            System.out.println("사칙연산 기호를 잘못 입력하셨습니다 다시 입력해주세요");
+                        } else {
+                            // 사칙연산 메서드 사용
+                            calculator.calculator(firstNum, secondNum, operator);
+                            break;
+                        }
+                    }
+                }
+                // 입력 받은 값을 사칙 연산 후
+                System.out.println("더 계산하시겠습니까? (exit 입력 시 종료, exit 외 입력 시 반복)");
+                if("exit".equals(sc.next())){
+                    break;
+                }else{
+                    continue;
+                }
+            }
+            catch (InputMismatchException e){
+                System.out.println("정수를 입력해주세요");
+                sc.nextLine(); // 버퍼 지우기
+            }
+
+        }
+
+
+
+
+
+
 
     }
 }
