67ad0bfea59867aa5bb9ea7aab8658f17d281ceb
DongGeun <kk506kk@naver.com>
2025-04-22 11:32:49 +0900

    LV1 수정

--- src/lv1/App.java
+++ src/lv1/App.java
@@ -20,56 +20,49 @@ public class App {
                 // 입력 받은 숫자가 0보다 작을 경우
                 if (firstNum < 0 || secondNum < 0) {
                     System.out.println("숫자를 0이상 입력해주세요!!");
-                    continue;
-                }else{
-                    boolean isTrue = true;
-                    while(isTrue){
-                        // 사칙연산 기호 입력 받기
-                        System.out.print("사칙연산 기호를 입력해주세요");
-                        char operator = sc.next().charAt(0);
-
-                        // 사칙연산 기호을 입력 받았을 경우
-                        if(operator == '+' || operator == '-' || operator == '*' || operator == '/'){
-                            switch (operator){
-                                case '+':
-                                    System.out.println(firstNum + " + " + secondNum + " = " + (firstNum + secondNum) );
-                                    isTrue = false;
-                                    break;
-                                case '-':
-                                    System.out.println(firstNum + " - " + secondNum + " = " + (firstNum - secondNum) );
-                                    isTrue = false;
-                                    break;
-                                case '*':
-                                    System.out.println(firstNum + " * " + secondNum + " = " + (firstNum * secondNum) );
-                                    isTrue = false;
-                                    break;
-                                case '/':
-                                    if(secondNum ==0){
-                                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력 될 수없습니다.");
-                                        System.out.println("사칙연산 기호를 다시 입력해주세요");
-                                        break;
-                                    }else{
-                                        System.out.println(firstNum + " / " + secondNum + " = " + (firstNum / secondNum) );
-                                        isTrue = false;
-                                        break;
-                                    }
-                            }
-                        }
-                        // 사칙연산 기호를 입력받지 못하였을 경우
-                        else {
-                            System.out.println("사칙연산 기호를 잘못 입력하셨습니다 다시 입력해주세요");
-                            continue;
-                        }
-                    }
-                    // 입력 받은 값을 사칙 연산 후
-                    System.out.println("더 계산하시겠습니까? (exit 입력 시 종료, exit 외 입력 시 반복)");
-                    if("exit".equals(sc.next())){
-                        break;
-                    }else{
-                        continue;
-                    }
                 }
 
+
+                while(true){
+                    // 사칙연산 기호 입력 받기
+                    System.out.print("사칙연산 기호를 입력해주세요");
+                    char operator = sc.next().charAt(0);
+
+                    // 사칙연산 기호을 입력 받았을 경우
+                    int result = 0;
+                    if(operator == '+' || operator == '-' || operator == '*' || operator == '/'){
+                        switch (operator){
+                            case '+':
+                                result = firstNum + secondNum;
+                                break;
+                            case '-':
+                                result = firstNum - secondNum;
+                                break;
+                            case '*':
+                                result = firstNum * secondNum;
+                                break;
+                            case '/':
+                                if(secondNum ==0){
+                                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력 될 수없습니다.");
+                                    System.out.println("사칙연산 기호를 다시 입력해주세요");
+                                    // while문 건너뜀
+                                    continue;
+                                }else{
+                                    result = firstNum / secondNum;
+                                    break;
+                                }
+                        }
+                        System.out.println(firstNum + " " + operator + " " + secondNum + " = " + result );
+                        break;
+                    }
+                    // 사칙연산 기호를 입력받지 못하였을 경우
+                    System.out.println("사칙연산 기호를 잘못 입력하셨습니다 다시 입력해주세요");
+                }
+                // 입력 받은 값을 사칙 연산 후
+                System.out.println("더 계산하시겠습니까? (exit 입력 시 종료, exit 외 입력 시 반복)");
+                if("exit".equals(sc.next())){
+                    break;
+                }
             }
             // 예외 발생 시 (정수 외에 입력 하였을 경우)
             catch(InputMismatchException e){
